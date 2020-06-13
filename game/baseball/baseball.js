89beb62a5ae42e97e5d3b8c44876625032f9a3b3
min33sky <min33sky@naver.com>
2020-06-13 17:42:23 +0900

    Baseball Updated

--- game/baseball/baseball.js
+++ game/baseball/baseball.js
@@ -1,60 +1,91 @@
 /**
- * 숫 자 야 구
+ * 숫자야구 리뉴얼
  */
-const form = document.querySelector('.form');
-const result = document.querySelector('.result');
-const input = document.querySelector('.input');
-const button = document.querySelector('.btn');
-const MAX = 10; // 10번의 기회
-
-let randomBaseballNumber; // 랜덤 숫자를 담을 배열
-let count = 0;
-getRandomNumber();
-
-form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  let data = Number(input.value);
-
-  if (data === Number(randomBaseballNumber.join(''))) {
-    console.log('정답');
-    input.value = '';
-    input.focus();
-    getRandomNumber();
-    count = 0;
-    result.textContent = '정 답 :) 한번 더 ??';
-  } else {
-    // 정답이 아닐 경우
-    let ball = 0;
-    let strike = 0;
-    for (let i = 0; i < randomBaseballNumber.length; i++) {
-      if (Number(input.value[i]) === randomBaseballNumber[i]) {
-        strike++;
-      } else if (randomBaseballNumber.indexOf(Number(input.value[i])) > -1) {
-        ball++;
-      }
-    }
-    count++;
-    if (count === MAX) {
-      result.textContent = `탈락! (정답: ${randomBaseballNumber})`;
-      count = 0;
-    } else {
-      result.textContent = `${strike}스트라이크 ${ball}볼 - (${MAX -
-        count}번의 기회가 남았습니다.)`;
-    }
-  }
-});
 
 /**
- * 랜덤 숫자 생성
+ * TODO : 횟수 제한 기능 구현, 중복된 숫자 입력 막기
  */
-function getRandomNumber() {
-  let substitute = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let result = [];
-  for (let i = 0; i < 4; i++) {
-    let idx = Math.floor(Math.random() * substitute.length);
-    let number = substitute.splice(idx, 1)[0];
-    result.push(number);
+
+function init() {
+  const Log = document.querySelector('#log');
+  const Form = document.querySelector('form');
+  const Input = document.querySelector('.input');
+
+  let ANSWER = setBaseballNumber(); // 정답을 담은 배열
+  let inputValue = ''; // 입력 값
+
+  console.log('정답 : ', ANSWER);
+
+  Input.focus();
+
+  Form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = Input.value;
+    inputValue = value;
+
+    if (inputValue === ANSWER.join('')) {
+      const div = document.createElement('div');
+      div.style.color = 'red';
+      div.innerText = '홈런.....';
+
+      Log.append(div, document.createElement('br'));
+      Log.append('3초후 리셋', document.createElement('br'));
+      setTimeout(() => {
+        reset();
+      }, 3000);
+    } else {
+      const inputValueArr = inputValue.split('').map((v) => Number(v));
+      let strike = 0;
+      let ball = 0;
+
+      for (const [aIndex, aValue] of ANSWER.entries()) {
+        for (const [inputIndex, inputValue] of inputValueArr.entries()) {
+          // 값이 일치하면 최소 볼, 위치까지 일치하면 스트라이크
+          if (aValue === inputValue && aIndex === inputIndex) {
+            strike++;
+          } else if (aValue === inputValue) {
+            ball++;
+          }
+        }
+      }
+
+      const div = document.createElement('div');
+      div.innerText = `${strike}스트라이크 ${ball}볼`;
+
+      Log.append(div, document.createElement('br'));
+    }
+
+    // Input 초기화 및 포커스
+    Input.value = '';
+    Input.focus();
+  });
+
+  function reset() {
+    ANSWER = setBaseballNumber();
+    inputValue = '';
+    Log.innerHTML = '';
+    console.log(ANSWER);
   }
-  console.log('정답: ', result);
-  randomBaseballNumber = result;
 }
+
+/**
+ * 정답을 생성하는 함수
+ */
+function setBaseballNumber() {
+  const result = [];
+
+  const candidadates = Array(10)
+    .fill(0)
+    .map((v, i) => i);
+
+  for (let i = 0; i < 4; i++) {
+    const index = Math.floor(Math.random() * candidadates.length);
+    result.push(candidadates.splice(index, 1)[0]);
+  }
+
+  return result;
+}
+
+// ***********************************************
+
+init();
