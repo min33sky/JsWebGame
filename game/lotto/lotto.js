ba59d5c04885a44f3868ca1b7dcfdabc9bfa2ad0
min33sky <min33sky@naver.com>
2020-06-14 17:45:42 +0900

    Lotto renewal Start

--- game/lotto/lotto.js
+++ game/lotto/lotto.js
@@ -1,54 +1,55 @@
-/**
- * 로 또 번 호 생 성 기
- */
+function init() {
+  console.log('Game Start...');
+  const containerDOM = document.querySelector('#container');
+  const lottoDOM = document.querySelector('.lotto');
+  const bonusDOM = document.querySelector('.bonus');
+  const COLORS = ['green', 'yellow', 'blue', 'red', 'orange', 'violet'];
 
-const lottoView = document.querySelector(".lotto");
-const bonusView = document.querySelector(".bonus");
+  const LOTTO = getLottoNumber(); // 로또번호 배열과 보너스 번호를 담은 객체
 
-//  후보군 배열 생성 (1~45)
-const substitutes = Array(45)
-  .fill(0)
-  .map((e, i) => i + 1);
+  console.log(LOTTO);
 
-const lottoNumbers = []; // 로또 번호를 담을 배열
-while (substitutes.length > 0) {
-  let idx = Math.floor(Math.random() * substitutes.length);
-  lottoNumbers.push(substitutes.splice(idx, 1)[0]);
-}
+  const interval = setInterval(() => {
+    const divDOM = document.createElement('div');
+    const color = COLORS[Math.floor(Math.random() * COLORS.length)];
+    divDOM.classList.add('number', color);
 
-const lottoResult = lottoNumbers.slice(0, 6).sort((a, b) => a - b);
-const bonusNumber = lottoNumbers[lottoNumbers.length - 1];
-
-for (let i = 0; i < lottoResult.length; i++) {
-  setTimeout(function() {
-    let div = document.createElement("div");
-    div.textContent = lottoResult[i];
-    div.id = "number";
-
-    if (lottoResult[i] <= 10) {
-      div.className = "red";
-    } else if (lottoResult[i] <= 20) {
-      div.className = "orange";
-    } else if (lottoResult[i] <= 30) {
-      div.className = "yellow";
-    } else if (lottoResult[i] <= 40) {
-      div.className = "blue";
+    if (LOTTO.lottoNumber.length === 0) {
+      clearInterval(interval);
+      divDOM.innerText = LOTTO.bonus;
+      bonusDOM.appendChild(divDOM);
     } else {
-      div.className = "green";
+      const number = LOTTO.lottoNumber.splice(0, 1)[0];
+      divDOM.innerText = number;
+      lottoDOM.appendChild(divDOM);
     }
-
-    lottoView.append(div);
-    // 보너스 번호 출력
-    if (i == lottoResult.length - 1) {
-      setTimeout(() => {
-        let div = document.createElement("div");
-        div.textContent = bonusNumber;
-        div.id = "number";
-        bonusView.append(div);
-      }, 1000);
-    }
-  }, 1000 * i);
+  }, 1000);
 }
 
-console.log(lottoNumbers);
-console.log(lottoResult, "보너스 번호: " + bonusNumber);
+/**
+ * 로또번호 생성 함수
+ */
+function getLottoNumber() {
+  const lotto = {};
+
+  const candidates = Array(45)
+    .fill(0)
+    .map((v, i) => i + 1);
+
+  const lottoNumber = [];
+
+  // candidates 배열의 값을 섞은 후 앞에서 6개는 로또번호 제일 뒷 번호는 보너스 번호로 설정
+  while (candidates.length) {
+    const index = Math.floor(Math.random() * candidates.length);
+    lottoNumber.push(candidates.splice(index, 1)[0]);
+  }
+
+  lotto.lottoNumber = lottoNumber.slice(0, 6);
+  lotto.bonus = lottoNumber[lottoNumber.length - 1];
+
+  return lotto;
+}
+
+// ***********************************
+
+init();
