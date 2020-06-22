40d6022fb5d0039dc5179a04ba5a10a27cc821bc
min33sky <min33sky@naver.com>
2020-06-22 17:46:47 +0900

    Update: Next Block view

--- game/Tetris_new/tetris.js
+++ game/Tetris_new/tetris.js
@@ -13,27 +13,13 @@ const gameDOM = document.querySelector('#game');
 const nextBlockDOM = document.querySelector('#next-block');
 const gameData = []; // 게임 진행 데이터가 저장될 배열
 
-const ROW = 20; // 테이블 세로
+const ROW = 24; // 테이블 세로
 const COL = 10; // 테이블 가로
 let timeOut = 0; // 타이머 관련 변수
 
-let currentBlock = null; // 현재 블록
-let currentTopLeft = [0, 3]; // 현재 위치
-
-/**
- * 이동할 수 있는 블록인지 확인하는 함수
- * @param {number} value
- */
-const isActiveBlock = (value) => value > 0 && value < 10;
-
-/**
- * 바닥을 지나간 블록이거나 이미 고정된 블록인지 확인하는 함수
- * @param {number} value
- */
-const isInvalidBlock = (value) => {
-  console.log('isInvalidBlock 호출', value);
-  return value === undefined || value >= 10;
-};
+let currentBlock = null;
+let nextBlock = null;
+let currentTopLeft = null; // 현재 블록 위치
 
 // 블록 색깔
 const BLOCK_COLORS = [
@@ -232,7 +218,8 @@ function init() {
   console.log('Game Start....');
 
   // 화면과 데이터 배열 생성
-  generateTable();
+  generateTable(true);
+  generateTable(false);
   // 블록 생성하기
   generateBlock();
   // 화면 그리기
@@ -241,66 +228,87 @@ function init() {
 
 /**
  * 게임 데이터를 담을 배열과 화면에 그릴 테이블을 생성하는 함수
+ *
+ * @param {boolean} option true 메인 화면, false 다음 블록 화면
  */
-function generateTable() {
+function generateTable(option) {
   /*
     ? fragment를 사용하는 것이 직접 DOM을 건드는 것보다 자원이 절약된다.
   */
   const tableDOM = document.createElement('table');
   const fragment = document.createDocumentFragment();
 
-  Array(ROW)
+  let tableRow = option ? ROW : 3;
+  let tableCol = option ? COL : 4;
+
+  Array(tableRow)
     .fill(0)
     .forEach(() => {
       const trDOM = document.createElement('tr');
-      Array(COL)
+      Array(tableCol)
         .fill(0)
         .forEach(() => {
           const tdDOM = document.createElement('td');
           trDOM.appendChild(tdDOM);
         });
       fragment.appendChild(trDOM);
-      gameData.push(Array(COL).fill(0));
+
+      option && gameData.push(Array(tableCol).fill(0));
     });
 
   tableDOM.appendChild(fragment);
-  gameDOM.appendChild(tableDOM);
-  console.log('현재 데이터', gameData);
+  option ? gameDOM.appendChild(tableDOM) : nextBlockDOM.appendChild(tableDOM);
 }
 
 /**
  * 블록 생성
  */
 function generateBlock() {
-  /*
-    TODO:
-    1. 다음 블록이 있을 경우 다음 블록이 현재 블록으로 교체된다.
-    2. 제일 첫 열을 제거한 후 출력
-  */
-  // 블록 생성하기
-  if (currentBlock) {
-    // nextBlock?
-  } else {
+  if (!currentBlock) {
     currentBlock = BLOCKS[Math.floor(Math.random() * BLOCKS.length)];
+  } else {
+    currentBlock = nextBlock;
   }
 
-  // 시작 위치에 현재 블록 출력하기 [0, 3]
-  console.log(currentBlock.shape[0]);
+  // 새로운 블록이 생성될 때마다 현재 블록 위치를 초기화 해준다.
+  currentTopLeft = [-1, 3];
 
   currentBlock.shape[0].slice(1).forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
       if (col) {
-        // gameData[rowIndex][colIndex + 3] = currentBlock.numCode;
         gameData[rowIndex][colIndex + 3] = currentBlock.numCode;
       }
     });
   });
 
-  console.log(gameData);
+  // 다음 블록을 생성한다.
+  generateNextBlock();
 }
 
 /**
- * 화면에 그려주는 함수
+ * 다음 블록을 생성하고 화면에 표시하는 함수
+ */
+function generateNextBlock() {
+  const nextBlockTable = document.querySelector('#next-block table');
+  nextBlock = BLOCKS[Math.floor(Math.random() * BLOCKS.length)];
+  const nextBlockShape = nextBlock.shape[0];
+
+  Array.from(nextBlockTable.childNodes).forEach((tr, i) => {
+    Array.from(tr.childNodes).forEach((td, j) => {
+      nextBlockTable.childNodes[i].childNodes[j].className = '';
+      if (nextBlockShape[i][j]) {
+        nextBlockTable.childNodes[i].childNodes[j].classList.add(
+          BLOCK_COLORS[nextBlock.numCode - 1],
+        );
+      } else {
+        nextBlockTable.childNodes[i].childNodes[j].className = '';
+      }
+    });
+  });
+}
+
+/**
+ * 화면에 블록을 그려주는 함수
  */
 function draw() {
   const tableDOM = document.querySelector('table');
@@ -327,13 +335,13 @@ function draw() {
  */
 function moveBlockDown() {
   /*
-    TODO
-    1. 블록이 아래까지 내려가면 다음 블록을 내린다.
+    TODO : 블록이 고정된 후에 삭제 가능한 행이 있는지 확인
+
    */
 
   const nextTopLeft = [currentTopLeft[0] + 1, currentTopLeft[1]]; // 현재 위치에서 한 칸 아래
-  const activeBlock = []; // 고정 시킬 블록 좌표들
-  let canGoDown = true; // 내려갈 수 있는 블록인지 판단하는 flag
+  const activeBlockCoords = []; // 현재 블록의 셀 좌표
+  let moveableBlock = true;
   let currentBlockShape = currentBlock.shape[currentBlock.currentShapeIndex]; // 현재 블록 모양
 
   for (
@@ -353,30 +361,35 @@ function moveBlockDown() {
        * 2. 현재 블록의 다음 위치가 움직일 수 있는지 확인하기 (바닥이거나 이미 블록이 있거나)
        */
       if (isActiveBlock(gameData[i][j])) {
-        console.log('호출', i, j);
         // 현재 블록은 움직일 수 있으므로 배열에 담아둔다.
-        activeBlock.push([i, j]);
+        activeBlockCoords.push([i, j]);
         // 인덱스가 바닥을 넘어갈 경우 undefined
         if (isInvalidBlock(gameData[i + 1] && gameData[i + 1][j])) {
-          console.log('못움직여', i + 1, gameData[i + 1]);
-          canGoDown = false;
+          moveableBlock = false;
         }
       }
     }
   }
 
-  if (!canGoDown) {
+  if (!moveableBlock) {
     // 움직일 수 없을 때 블록을 고정시키는 처리
-    activeBlock.forEach((cell) => {
+    activeBlockCoords.forEach((cell) => {
       gameData[cell[0]][cell[1]] = gameData[cell[0]][cell[1]] * 10;
     });
+
+    // TODO: 지울 블록이 있는지 확인
+
+    // 다음 블록을 셍상
+    generateBlock();
   } else {
-    // * 아래로 내려갈 수 있는 블록 처리
+    /*
+     * 고정되지 않고 아래로 움직일 수 있는 블록일 경우
+     * 아래 칸으로 이동시키고 현재 칸은 지워준다.
+     */
     for (let i = ROW - 1; i >= 0; i--) {
       for (let j = 0; j < COL; j++) {
         if (gameData[i][j] > 0 && gameData[i + 1] && gameData[i][j] < 10) {
           gameData[i + 1][j] = gameData[i][j];
-          // 현재 위치의 값은 지워준다.
           gameData[i][j] = 0;
         }
       }
@@ -386,7 +399,22 @@ function moveBlockDown() {
   }
 }
 
-// ***************************************
+/**
+ * 이동할 수 있는 블록인지 확인하는 함수
+ * @param {number} value
+ */
+const isActiveBlock = (value) => value > 0 && value < 10;
+
+/**
+ * 바닥을 지나간 블록이거나 이미 고정된 블록인지 확인하는 함수
+ * @param {number || undefined} value
+ */
+const isInvalidBlock = (value) => {
+  // console.log('isInvalidBlock 호출', value);
+  return value === undefined || value >= 10;
+};
+
+// ************************************************************************
 
 init();
 timeOut = setInterval(moveBlockDown, 200);
